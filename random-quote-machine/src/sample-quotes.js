7c7233bf24395c30a14c345a94fc847d03b3d955
Troy Jennings <troy.c.jennings@gmail.com>
2018-12-13 21:51:40 +0100

    sample quotes added

---
+++ random-quote-machine/src/sample-quotes.js
@@ -0,0 +1,22 @@
+const quotes = {
+    quote1: {
+        author: "Larry David",
+        quote: "I'm feeling pretty good. Pret-ty, pret-ty, pret-ty, pret-ty good."
+    },
+    quote2: {
+        author: "Larry David",
+        quote: "You know who wears sunglasses inside? Blind people and assholes."
+    },
+    quote3: {
+        author: "Larry David",
+        quote: "Funkhouser knowingly served us penis!"
+    },
+    quote4: {
+        author: "Larry David",
+        quote: "You're black and your last name is Black? That's like if my name was Larry Jew."
+    },
+    quote5: {
+        author: "Larry David",
+        quote: "F**k Hhhhugh."
+    },
+};
