ee0192a940442aa7f65d7d6ff429623002980efc
Troy Jennings <troy.c.jennings@gmail.com>
2018-12-15 20:08:42 +0100

    - Added quote box styling:   - Font and font size   - Div positioning   - Button styling   - Added styling class names to Quote.js

--- random-quote-machine/src/components/Quote.js
+++ random-quote-machine/src/components/Quote.js
@@ -1,23 +1,28 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Quote extends React.Component {
     static propTypes = {
-
+      quote: PropTypes.string,
+      author: PropTypes.string
     }
 
     render() {
         return <div id="quote-box" className="quote-box">
-            <h1 id="text" className="text">
-              {this.props.quote}
+            <h1 id="text" className="quote-box__text">
+              "{this.props.quote}"
             </h1>
-            <p id="author" className="author">
-              {this.props.author}
+            <p id="author" className="quote-box__author">
+              - {this.props.author}
             </p>
-            <button id="new-quote" className="new-quote btn btn-default" onClick={this.props.loadQuote}>
-              Get New Quote
-            </button>
-            <a id="tweet-quote" className="btn btn-default" href="twitter.com/intent/tweet">Tweet Quote</a>
+            <div className="btn-container">
+              <button id="new-quote" className="quote-box__new-quote" onClick={this.props.loadQuote}>
+                <img src="/assets/larry.png" className="new-quote-img" title="Click for a new quote!" alt="larry head"/>
+              </button>
+              <a id="tweet-quote" className="quote-box__tweet btn btn-default" href="twitter.com/intent/tweet">
+                <img src="/assets/twitter-icon.png" className="tweet-img" title="Tweet quote!" alt="twitter icon" />
+              </a>
+            </div>
           </div>;
     }
 }
