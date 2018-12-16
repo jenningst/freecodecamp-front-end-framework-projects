bf3ac0b43b15ae994119a0ec66d10e8731c5cfa1
Troy Jennings <troy.c.jennings@gmail.com>
2018-12-16 19:24:17 +0100

    Added Imports for Components
    - Fixed compile error due to missing imports

--- markdown-previewer/src/components/App.js
+++ markdown-previewer/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Editor from './Editor';
+import Previewer from './Previewer';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="wrapper">
                 <Editor />
                 <Previewer />
             </div>
