61422101656fe817622af7b4168e1bcb64aadf5b
Troy Jennings <troy.c.jennings@gmail.com>
2018-12-27 17:58:49 +0100

    FEAT: Adds Increment/Decrement
    - Break and Session incrementers allow for increment/decrement on click
    - Min/max threshold of 0 and 60 in place
    - Initial handleReset in place

--- pomodoro-timer/src/components/App.js
+++ pomodoro-timer/src/components/App.js
@@ -4,12 +4,34 @@ import PropTypes from 'prop-types';
 
 class App extends Component {
     state = {
-        breakLength: 300,
-        sessionLength: 1500,
+        break: 300,
+        session: 1500,
         remainingTime: 0,
         pause: false,
         toggleSession: true,
         toggleStart: true
+    }
+
+    handleIncrement = (e) => {
+        const stateTarget = e.currentTarget.id.split('-')[0];
+        const incrementer = e.currentTarget.id.split('-')[1];
+        let currentLength = this.state[stateTarget];
+        const newLength = () => incrementer === 'increment' ? currentLength + 60 : currentLength - 60;
+        
+        if (currentLength >= 60 && currentLength <= 3540) {
+            this.setState({ [stateTarget] : newLength() });
+        }
+    }
+
+    handleReset = (e) => {
+        this.setState({
+            break: 300,
+            session: 1500,
+            remainingTime: 0,
+            pause: false,
+            toggleSession: true,
+            toggleStart: true
+        })
     }
 
     render() {
@@ -20,17 +42,17 @@ class App extends Component {
                     <div className="break-controls">
                         <p id="break-label">Break Length</p>
                         <div className="breakrementer">
-                            <div id="break-decrement" />
-                            <div id="break-length">{formatTime(this.state.breakLength)}</div>
-                            <div id="break-increment" />
+                            <button id="break-decrement" onClick={this.handleIncrement}>-</button>
+                            <div id="break-length">{formatTime(this.state.break)}</div>
+                            <button id="break-increment" onClick={this.handleIncrement}>+</button>
                         </div>
                     </div>
                     <div className="session-controls">
                         <p id="session-label">Session Length</p>
                         <div className="sessionrementer">
-                            <div id="session-decrement" />
-                            <div id="session-length">{formatTime(this.state.sessionLength)}</div>
-                            <div id="session-increment" />
+                            <button id="session-decrement" onClick={this.handleIncrement}>-</button>
+                            <div id="session-length">{formatTime(this.state.session)}</div>
+                            <button id="session-increment" onClick={this.handleIncrement}>+</button>
                         </div>
                     </div>
                 </div>
@@ -40,7 +62,7 @@ class App extends Component {
                 </div>
                 <div className="timer-buttons">
                     <button id="start_stop">{this.state.toggleStart ? 'Start' : 'Stop'}</button>
-                    <button id="reset">Reset</button>
+                    <button id="reset" onClick={this.handleReset}>Reset</button>
                 </div>
                 </div>
             </div>
